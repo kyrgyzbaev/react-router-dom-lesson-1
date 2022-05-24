e67b0a6f7073bf41623a2ba8c83c89673e613183
kyrgyzbaev <baygazykyrgyzbaev97@gmail.com>
2022-05-23 21:27:55 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Header from './components/Header/Header';
+import Filters from './components/Filters/Filters';
+import ProductsList from './components/ProductsList/ProductsList';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header />
+    <h1>Header</h1>
+    <Routes>
+      <Route path='/' element={ <><Filters /> <ProductsList /></> } />
+      <Route path='/add' element={<h1>Add form</h1>} />
+      <Route path='/edit' element={<h1>Edit form</h1>} />
+      <Route path='/contacts' element={<h1>Contacts</h1>} />
+    </Routes>
+    <h1>Footer</h1>
+    </BrowserRouter>
+  );
+}
+
+export default App;
